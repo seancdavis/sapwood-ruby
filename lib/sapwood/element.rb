5c5bdabb08f58ce8a5fb161a3aaaf962c0a074c7
Sean C Davis <scdavis41@gmail.com>
2016-12-09 08:21:33 -0500

    support hashes (as mashes) and element collections

--- lib/sapwood/element.rb
+++ lib/sapwood/element.rb
@@ -7,10 +7,20 @@ module Sapwood
       @attributes = attrs.symbolize_keys
     end
 
+    def respond_to?(method)
+      @attributes.include?(method.to_sym)
+    end
+
     def method_missing(method, *args, &block)
       if @attributes.keys.include?(method.to_sym)
         value = @attributes[method.to_sym]
-        return value.is_a?(Hash) ? Hashie::Mash.new(value) : value
+        if value.is_a?(Array)
+          return value.map { |v| Sapwood::Element.new(v) }
+        elsif value.is_a?(Hash)
+          return Hashie::Mash.new(value)
+        else
+          return value
+        end
       end
       super
     end
